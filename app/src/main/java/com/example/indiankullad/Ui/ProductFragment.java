4eb4e3c54e662e0575d44918eb0e535c9c6e5551
anjalisingh345 <shivamroyal478@gmail.com>
2020-12-23 13:49:51 +0530

    kullhad

--- app/src/main/java/com/example/indiankullad/Ui/ProductFragment.java
+++ app/src/main/java/com/example/indiankullad/Ui/ProductFragment.java
@@ -9,11 +9,16 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.indiankullad.Model;
+import com.example.indiankullad.Product_Adapter;
 import com.example.indiankullad.R;
+
+import java.util.List;
 
 public  class ProductFragment extends Fragment {
 
     RecyclerView rc;
+    List<Model>mdata;
 
 
     @Override
@@ -25,6 +30,8 @@ public  class ProductFragment extends Fragment {
         rc = view.findViewById(R.id.recycler);
         rc.setHasFixedSize(true);
         rc.setLayoutManager(new LinearLayoutManager(getContext()));
+        Product_Adapter adapter = new Product_Adapter(getContext(),mdata);
+        rc.setAdapter(adapter);
 
         return view;
 
