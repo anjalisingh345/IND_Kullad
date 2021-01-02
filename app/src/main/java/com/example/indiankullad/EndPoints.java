f7b46ca26379c08de3429a9881c52864b033a1e0
anjalisingh345 <shivamroyal478@gmail.com>
2021-01-02 10:54:15 +0530

    kullhad

---
+++ app/src/main/java/com/example/indiankullad/EndPoints.java
@@ -0,0 +1,6 @@
+package com.example.indiankullad;
+
+public class EndPoints {
+
+    public  static String REGISTER = "https://anjalicloud2712.000webhostapp.com/Kullad/Insert.php";
+}
--- app/src/main/java/com/example/indiankullad/Ui/ProductFragment.java
+++ app/src/main/java/com/example/indiankullad/Ui/ProductFragment.java
@@ -7,23 +7,44 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.indiankullad.EndPoints;
 import com.example.indiankullad.Model;
 import com.example.indiankullad.Product_Adapter;
 import com.example.indiankullad.R;
+import com.example.indiankullad.Util;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public  class ProductFragment extends Fragment {
     RecyclerView.LayoutManager layoutManager;
@@ -89,7 +110,7 @@ public  class ProductFragment extends Fragment {
                 @Override
                 public void onClick(View view) {
                     //call recreate method
-                    recreate();
+                //    recreate();
 
 
                 }
@@ -102,8 +123,93 @@ public  class ProductFragment extends Fragment {
             dialog.show();
         } else {
 
+            //for getting place information
+
+            Util.showProgressDialog(getActivity(), false);
+            requestQueue = Volley.newRequestQueue(getActivity());
+            StringRequest request = new StringRequest(Request.Method.POST,
+                    EndPoints.REGISTER, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    Util.dismisProgressDialog();
+                    JSONArray jsonArray = null;
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        jsonArray = (JSONArray) jsonObject.get("data");
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+
+                            JSONObject obj = jsonArray.getJSONObject(i);
+
+
+//                            String[] separated = obj.getString("package").split(" ");
+//                            int myNum = 0;
+
+                            try {
+//                                myNum = Integer.parseInt(separated[0]);
+//                                Date c = Calendar.getInstance().getTime();
+//                                SimpleDateFormat df = new SimpleDateFormat("dd/MM/yy", Locale.ENGLISH);
+//                                String formattedDate = df.format(c);
+                                 String kullad_name = obj.getString("kullad_name");
+                                    //  String surname = obj.getString("surname");
+                                    String kullad_code = obj.getString("kullad_code");
+                                    String price = obj.getString("price");
+                                    String des = obj.getString("des");
+                                    String img=obj.getString("feepaid");
+                                    Model model = new Model();
+                                    model.setKullad_name(kullad_name);
+                                    model.setKullad_code(kullad_code);
+                                    model.setPrice(price);
+                                    model.setDescription(des);
+                                    model.setImage(dob);
+                                    model.setGender(gender);
+                                    model.setAddress(address);
+                                    model.setJoindate(joindate);
+                                    model.setMid(mid);
+                                    model.setContact(contact);
+                                    model.setTotalfee(totalfee);
+                                    //model.setGettoknow(gettoknow);
+                                    model.setImage(img);
+                                    model.setFeepaid(feepaid);
+
+                                    mydata.add(model);
+
+
+
+
+                            } catch (NumberFormatException nfe) {
+
+                            }
+
+
+                        }
+                    } catch (JSONException e) {
+                        Util.dismisProgressDialog();
+                        Toast.makeText(Duration_Recycler.this, "catch" + e.toString(), Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                    Duration_Adapter adapter = new Duration_Adapter(Duration_Recycler
+                            .this, mydata);
+                    rc.setAdapter(adapter);
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Util.dismisProgressDialog();
+                    Log.d("hhh", error.getMessage());
+
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("action", "readAll");
+                    return params;
+                }
+            };
+            requestQueue.add(request);
+
 
         }
-
-
-    }
